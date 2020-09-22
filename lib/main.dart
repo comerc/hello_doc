9436ea28c3070e42f538d14e60205862c4ec3cbf
comerc <andrew.kachanov@gmail.com>
2020-09-22 18:01:25 +0300

    Добавил TODO

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,20 @@ import 'views/index.dart' as views;
 import 'presenters/index.dart' as presenters;
 import 'presenters/presenter_provider.dart';
 
+// TODO: vertical orientation
+// TODO: скроллировать "Готово"
+// TODO: header elevation
+// TODO: форма в диалоге
+// TODO: сепараторы
+// TODO: AnimatedBox
+// TODO: device_preview
+// TODO: SafeArea
+// TODO: bot_toast для ошибок вне контекста Scaffold
+// TODO: import.dart
+// TODO: StartScreen для initialRoute
+// TODO: MediaQueryWrap
+// TODO: appBarTheme
+
 void main() {
   runApp(PresenterProvider<presenters.Schedule>(
     child: views.Schedule(),
